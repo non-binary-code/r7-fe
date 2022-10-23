0fcc7a4c01285add86a4ff563f11e634233af1c5
Zan Clifton <ZanClifton@users.noreply.github.com>
2022-10-23 20:08:57 +0100

    amends description placeholder

--- r7-fe/src/components/Form.jsx
+++ r7-fe/src/components/Form.jsx
@@ -51,7 +51,7 @@ const Form = () => {
             className='appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
             id='grid-short-description'
             type='text'
-            placeholder='This ia a really good microwave'
+            placeholder='This is a really good microwave'
             onChange={(e) => setItemDesc(e.target.value)}
           />
           {!itemDesc && (
