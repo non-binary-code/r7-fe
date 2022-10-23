6e7c27b8ae3173e3f4b9285a36bf26616553986f
Zan Clifton <ZanClifton@users.noreply.github.com>
2022-10-23 11:26:50 +0100

    adds some styling

--- r7-fe/src/components/Item.jsx
+++ r7-fe/src/components/Item.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { useLocation } from "react-router-dom";
 import { useStateContext } from "../context/ContextProvider";
+import { conditionIds } from "../utils/condition-config";
+import { categoryIds } from "../utils/category-config";
 
 const Item = () => {
   const location = useLocation();
@@ -10,20 +12,51 @@ const Item = () => {
 
   const alertText = `Do you really need this item?
   
-Remember you will need to store it until you are finished with it and then manage its disposal or re-gifting!`
+  Remember you will need to store it until you are finished with it and then manage its disposal or re-gifting!`
 
   return (
     <>
-      <h1>{item?.name}</h1>
-      <p>{item?.description}</p>
-      <img src={item?.image} alt={item?.name} />
-      <p>{item?.category}</p>
-      <p>{item?.condition}</p>
-      <p>{item?.delivery}</p>
-      <button onClick={() => {
-      window.confirm(alertText) ? setBasketItems((prev) => [...prev, item]) : setBasketItems((prev) => [...prev]) }}>
-        Add to basket
-      </button>
+      <div className='bg-gray-100 flex flex-col gap-10'>
+        <div className='flex m-3 flex-col flex-wrap items-start p-5'>
+
+          <div className=' gap-10 p-5'>
+            <h1 className='text-3xl font-bold mb-0'>{item?.name}</h1>
+            <p className='font-bold'>{categoryIds[item?.categoryTypeId - 1]}</p>
+          </div>
+
+          <div className='flex   flex-wrap gap-10 items-center p-5'>
+            <img
+              src={
+                item.pictureUrl
+                  ? item.pictureUrl
+                  : 'https://st4.depositphotos.com/14953852/24787/v/600/depositphotos_247872612-stock-illustration-no-image-available-icon-vector.jpg'
+              }
+              alt={item.name}
+              style={{ width: '200px', height: '200px' }}
+            />
+
+            <div>
+              <p className='font-bold'>{item?.description}</p>
+            </div>
+
+          </div>
+
+        </div>
+        <div className='flex gap-3 items-center p-5 pt-0'>
+          <div className='flex flex-col gap-3 p-5 py-0'>
+            <p className=' font-bold'>{conditionIds[item?.conditionTypeId - 1]}</p>
+            <p className='font-bold'>Delivery: {item.delivery ? "Available" : "Not Available"}</p>
+            <p className='font-bold'>Collection: {item.collection ? "Available" : "Not Available"}</p>
+            <p className='font-bold'>Postage: {item.postage ? "Available" : "Not Available"}</p>
+            <p className='font-bold'>Recover: {item.recover ? "Arranged by current owner" : "To be arranged by recipient"}</p>
+          </div>
+        </div>
+        <button className='ml-12 bg-green-400 hover:bg-green-700 text-white font-bold py-2 px-4 rounded' onClick={() => {
+          window.confirm(alertText) ? setBasketItems((prev) => [...prev, item]) : setBasketItems((prev) => [...prev])
+        }}>
+          Add to basket
+        </button>
+      </div>
     </>
   );
 };
