a3f6a9c0e5bd18b0e3f7b22ec5c7d5adf7c26ff1
Dane <danejwhitfield@gmail.com>
2022-10-23 22:33:13 +0100

    removed home

--- r7-fe/src/pages/Home.jsx
+++ r7-fe/src/pages/Home.jsx
@@ -1,12 +1,11 @@
 import Marketplace from './Marketplace';
 
 const Home = () => {
-	return (
-		<div>
-			<h1 className='text-3xl font-bold'>Home</h1>
-			<Marketplace />
-		</div>
-	);
+  return (
+    <div>
+      <Marketplace />
+    </div>
+  );
 };
 
 export default Home;
